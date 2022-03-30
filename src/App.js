609bf360d6a50be254b2de95733e6509f57d3fb5
Ng Chan Kok <ngchankok@gmail.com>
2022-03-30 21:12:13 +0800

    test deploy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React is Fun
           {/* <div>BaseURL: {config.baseURL}</div> */}
         </a>
       </header>
