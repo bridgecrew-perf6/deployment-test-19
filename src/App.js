146b85950c15c61af4d5d7eeb4154ded00bd4a10
Ng Chan Kok <ngchankok@gmail.com>
2022-03-30 20:53:36 +0800

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import config from './config';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (Deloy Test)
+          <div>BaseURL: {config.baseURL}</div>
         </a>
       </header>
     </div>
